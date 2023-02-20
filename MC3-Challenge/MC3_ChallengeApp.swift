e01e08efe3afb8a380a8378eed944cd2bbaa6b90
elyahk-apple-academy <enusratov22@fed.idserve.net>
2023-02-20 15:24:37 +0100

    Create repository

---
+++ MC3-Challenge/MC3_ChallengeApp.swift
@@ -0,0 +1,20 @@
+//
+//  MC3_ChallengeApp.swift
+//  MC3-Challenge
+//
+//  Created by Eldorbek Nusratov on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MC3_ChallengeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
