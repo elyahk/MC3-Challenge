770491b24ce7953308b1ecdf08eb1552b881661a
elyahk-apple-academy <enusratov22@fed.idserve.net>
2023-02-21 15:47:48 +0100

    Move MessageView to its own file

--- MC3-Challenge/ChatbotView.swift
+++ MC3-Challenge/ChatbotView.swift
@@ -80,33 +80,6 @@ struct ChatbotView: View {
     }
 }
 
-struct MessageView: View {
-    @ObservedObject var message: Message
-    
-    init(message: Message) {
-        self.message = message
-    }
-    
-    var body: some View {
-        HStack {
-            switch message.owner {
-            case .chatbot:
-                Text(message.content)
-                    .background(Color.yellow)
-                    .padding([.trailing], 100)
-                Spacer()
-            case .user:
-                Spacer()
-                Text(message.content)
-                    .background(Color.yellow)
-                    .padding([.leading], 100)
-            }
-            
-        }
-        .padding(4)
-    }
-}
-
 struct ChatbotView_Previews: PreviewProvider {
     static var previews: some View {
         ChatbotView(messageManager: .mock)
---
+++ MC3-Challenge/MessageView.swift
@@ -0,0 +1,41 @@
+//
+//  MessageView.swift
+//  MC3-Challenge
+//
+//  Created by Eldorbek Nusratov on 21/02/23.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    @ObservedObject var message: Message
+    
+    init(message: Message) {
+        self.message = message
+    }
+    
+    var body: some View {
+        HStack {
+            switch message.owner {
+            case .chatbot:
+                Text(message.content)
+                    .background(Color.yellow)
+                    .padding([.trailing], 100)
+                Spacer()
+            case .user:
+                Spacer()
+                Text(message.content)
+                    .background(Color.yellow)
+                    .padding([.leading], 100)
+            }
+            
+        }
+        .padding(4)
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageView(message: .init(content: "Message 1", owner: .chatbot))
+    }
+}
