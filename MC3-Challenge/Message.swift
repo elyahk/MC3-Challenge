6a454c07677b29e8b995a1ef6754df3dab0f8047
Ilona Sellenberk <ilona.sell@gmail.com>
2023-02-21 20:26:32 +0100

    add json file, parse from Json - handling errors

--- MC3-Challenge/Message.swift
+++ MC3-Challenge/Message.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-enum Owner: Equatable {
-    case chatbot
+enum Owner: String, Equatable, Decodable {
+    case bot
     case user
 }
 
-class Message: ObservableObject, Identifiable, Equatable {
+class Message: ObservableObject, Identifiable, Equatable, Decodable {
     var content: String
     var owner: Owner
     var id: String = ""
@@ -24,20 +24,39 @@ class Message: ObservableObject, Identifiable, Equatable {
         self.owner = owner
     }
     
+    // initialize decoder...
+    enum CodingKeys: CodingKey {
+        case content
+        case owner
+        case id
+        case options
+        case date
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.content = try container.decode(String.self, forKey: .content)
+        self.owner = try container.decode(Owner.self, forKey: .owner)
+        self.id = try container.decode(String.self, forKey: .id)
+        let options = try? container.decode([[String : String]].self, forKey: .options)
+        self.options = options ?? [] // optional can be empty
+        self.date = .init() // date is not in json, just initialize here
+    }
+    
     static func == (lhs: Message, rhs: Message) -> Bool {
         lhs.id == rhs.id
     }
 }
 
 extension Message {
-    static func long(owner: Owner = .chatbot) -> Message {
+    static func long(owner: Owner = .bot) -> Message {
         .init(
             content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, industry's standard dummy text ever since the 1500s",
             owner: owner
         )
     }
     
-    static func short(owner: Owner = .chatbot) -> Message {
+    static func short(owner: Owner = .bot) -> Message {
         .init(
             content: "Lorem Ipsum is simply",
             owner: owner
--- MC3-Challenge/MessageManager.swift
+++ MC3-Challenge/MessageManager.swift
@@ -6,4 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
 
+struct MessageJSON: Decodable {
+    var messages: [Message]
+}
+
+func loadJSONMessages() {
+    // 1. make connection to the json file
+    let data: Data
+    let loadedJson: MessageJSON
+    
+    guard let file = Bundle.main.url(forResource: "Message", withExtension: "json")
+        else {
+            fatalError("Couldn't find 'Message.json' from Bundle.main.")
+        }
+    
+    do {
+        data = try Data.init(contentsOf: file) // ! without do/catch
+    } catch {
+        fatalError("Couldn't load file Message.json from Bundle.main")
+    }
+    
+    do {
+        loadedJson = try JSONDecoder().decode(MessageJSON.self, from: data) // ? need declare variable to reach in do/catch
+    } catch {
+        fatalError("Couldn't decode/parse Message.json into MessageJSON struct.")
+    }
+    
+    print(loadedJson.messages[0].content as Any)
+}
